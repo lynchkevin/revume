30a4fcccd863a6016a78926d72a9a50d7f3629bb
Kevin Lynch <klynch@volerro.com>
2015-07-01 21:37:43 -0500

    added a logout function
    remove the cookie and ye be logged out

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -8,13 +8,19 @@
  * Controller of the barebonesApp
  */
 angular.module('starter')
-  .controller('settingsCtrl', ['$scope', '$rootScope','$state',
-                             function ($scope,$rootScope,$state) {
+  .controller('settingsCtrl', ['$scope', '$rootScope','$state', '$cookieStore',
+                             function ($scope,$rootScope,$state,$cookieStore) {
 
+    $scope.logOut = function(){
+        $cookieStore.remove('user');
+    };
     $scope.changePassword = function(){
         $state.go('app.changePassword');
     };
                                  
-    $scope.setting = {actions:[{name:'Change My Password',action:$scope.changePassword}]};
+    $scope.setting = {actions:[
+                                {name:'Log Out',action:$scope.logOut},
+                                {name:'Change My Password',action:$scope.changePassword}
+                      ]};
                                 
   }]);
