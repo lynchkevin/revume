d8f8d8206a4e0be2b66b7aab56035d7ee3cccd30
Kevin Lynch <klynch@volerro.com>
2015-07-08 15:34:56 -0500

    remove slidebox pager

--- server/gruntfile.js
+++ server/gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
         constants: {
           baseUrl: {
             name: 'production',
-            endpoint: 'https://m.revu.me',
+            endpoint: 'http://m.revu.me',
             volerro: 'https://rb.volerro.com'
           }
         }
