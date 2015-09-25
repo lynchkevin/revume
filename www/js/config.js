4bfa397870cd5f2ebaea27e07b1f234c396d34df
Kevin Lynch <klynch@volerro.com>
2015-09-25 11:22:08 -0500

    apple store first try

--- www/js/config.js
+++ www/js/config.js
@@ -2,10 +2,10 @@
 
  angular.module('config', [])
 
-.constant('baseUrl', {name:'development',endpoint:'http://10.1.10.216:5000',volerro:'https://rb.volerro.com'})
+.constant('baseUrl', {name:'production',endpoint:'https://m.revu.me',volerro:'https://rb.volerro.com'})
 
-.constant('buildDate', 'Mon Sep 21 2015 16:51:10 GMT-0500 (CDT)')
+.constant('buildDate', 'Thu Sep 24 2015 13:56:58 GMT-0500 (CDT)')
 
-.constant('clientTokenPath', 'http://10.1.10.216:5000/api/braintree/client_token')
+.constant('clientTokenPath', 'https://m.revu.me/api/braintree/client_token')
 
 ;
